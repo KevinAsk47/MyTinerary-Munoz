032477315ea0e9858845f8359f9f1798ebdb15d2
kevinAsk47 <kevinmunozmendez@gmail.com>
2021-05-20 23:34:23 -0300

    sad

--- server.js
+++ server.js
@@ -25,4 +25,4 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT
 const host = process.env.HOST || '0.0.0.0'
 
-app.listen(port, host, () => console.log("Estoy escuchando"))
+app.listen(port, host, () => console.log("Estoy escuchando" + port + host))
